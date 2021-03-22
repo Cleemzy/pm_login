92bda726ed0e1a60d9d1c0d03589617007ceae89
Cleemzy <razafintsalama.rmh@gmail.com>
2021-03-22 11:47:08 +0300

    created login page layout

--- lib/pm_login_web/controllers/page_controller.ex
+++ lib/pm_login_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule PmLoginWeb.PageController do
   use PmLoginWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", layout: {PmLoginWeb.LayoutView, "login_layout.html"})
   end
 end
