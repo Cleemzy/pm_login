4488294f16f8b029b3718d566bb2a241396c8187
Cleemzy <razafintsalama.rmh@gmail.com>
2021-04-06 19:40:57 +0300

    added modal as modalcomponent for confirming archiving user

--- lib/pm_login_web/live/user/list_live.ex
+++ lib/pm_login_web/live/user/list_live.ex
@@ -3,6 +3,7 @@ defmodule PmLoginWeb.User.ListLive do
   alias PmLogin.Login
   alias PmLoginWeb.UserView
   alias PmLoginWeb.UserController
+  alias PmLoginWeb.LiveComponent.ModalLive
 
   def mount(_params, %{"curr_user_id" => curr_user_id}, socket) do
     Login.subscribe()
@@ -17,15 +18,41 @@ defmodule PmLoginWeb.User.ListLive do
     user = Login.get_user!(id)
     Login.archive_user(user)
     # Log.delete_user(user)
-    {:noreply, fetch(socket)}
+
+    {:noreply, put_flash(socket, :info, "L'utilisateur #{user.username} a bien été archivé")}
   end
 
   defp fetch(socket) do
-    assign(socket, users: Login.list_all_auth(),layout: {PmLoginWeb.LayoutView, "admin_layout.html"})
+    assign(socket, users: Login.list_all_auth(),show_modal: false, arch_id: nil,layout: {PmLoginWeb.LayoutView, "admin_layout_live.html"})
   end
 
   def render(assigns) do
    UserView.render("index.html", assigns)
   end
+
+  #modal_component
+
+  def handle_info(
+      {ModalLive, :button_clicked, %{action: "cancel-arch"}},
+      socket
+    ) do
+  {:noreply, assign(socket, show_modal: false)}
+  end
+
+  def handle_info(
+      {ModalLive, :button_clicked, %{action: "arch", param: arch_id}},
+      socket
+    ) do
+      user = Login.get_user!(arch_id)
+      Login.archive_user(user)
+  {:noreply, assign(socket, show_modal: false)}
+  end
+
+  def handle_event("go-arch", %{"id" => id}, socket) do
+    # Phoenix.LiveView.get_connect_info(socket)
+    # put_session(socket, del_id: id)
+    {:noreply, assign(socket, show_modal: true, arch_id: id)}
+  end
+
 
 end
