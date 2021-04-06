c5eb772d68568ccde6ce9412fcccc5901e1e9963
Cleemzy <razafintsalama.rmh@gmail.com>
2021-04-06 20:29:18 +0300

    added js hook ScrollLock for modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,42 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 
+
+// Define hooks
+const Hooks = {}
+Hooks.ScrollLock = {
+  mounted() {
+    this.lockScroll()
+  },
+  destroyed() {
+    this.unlockScroll()
+  },
+  lockScroll() {
+    // From https://github.com/excid3/tailwindcss-stimulus-components/blob/master/src/modal.js
+    // Add right padding to the body so the page doesn't shift when we disable scrolling
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth
+    document.body.style.paddingRight = `${scrollbarWidth}px`
+    // Save the scroll position
+    this.scrollPosition = window.pageYOffset || document.body.scrollTop
+    // Add classes to body to fix its position
+    document.body.classList.add('fix-position')
+    // Add negative top position in order for body to stay in place
+    document.body.style.top = `-${this.scrollPosition}px`
+  },
+  unlockScroll() {
+    // From https://github.com/excid3/tailwindcss-stimulus-components/blob/master/src/modal.js
+    // Remove tweaks for scrollbar
+    document.body.style.paddingRight = null
+    // Remove classes from body to unfix position
+    document.body.classList.remove('fix-position')
+    // Restore the scroll position of the body before it got locked
+    document.documentElement.scrollTop = this.scrollPosition
+    // Remove the negative top inline style from body
+    document.body.style.top = null
+  }
+}
+//hook end
+
 let navToggle = document.querySelector(".nav__toggle");
 let navWrapper = document.querySelector(".nav__wrapper");
 navToggle.addEventListener("click", function () {
@@ -33,6 +69,13 @@ navToggle.addEventListener("click", function () {
 });
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+//Working liveSocket
+// let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {
+    _csrf_token: csrfToken
+  },
+  hooks: Hooks
+});
 
 liveSocket.connect()
