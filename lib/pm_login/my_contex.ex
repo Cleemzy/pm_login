f3b08768f8eb9e8d1464bac7f272ff760b39612d
Cleemzy <razafintsalama.rmh@gmail.com>
2021-06-14 18:00:06 +0300

    fixed gantt by adding css

--- lib/pm_login/my_contex.ex
+++ lib/pm_login/my_contex.ex
@@ -6,64 +6,140 @@ defmodule PmLogin.MyContex do
 
   def todays_dataset do
     todays = Monitoring.list_achieved_tasks_today
-
-    data = Enum.map(todays, fn(task) -> %{category_col: task.priority_id,
-          task_col: task.title, start_col: date_to_datetime(task.date_start), finish_col: naive_to_datetime(task.achieved_at)} end)
-    dataset = Contex.Dataset.new(data)
-
-    list = Enum.map(todays, fn(task) -> [task.priority_id, task.title, date_to_datetime(task.date_start), naive_to_datetime(task.achieved_at)] end)
-    # list = [[]]
-    list_dataset = Contex.Dataset.new(list)
-    IO.inspect list_dataset
-    # plot_content = Contex.GanttChart.new(dataset, mapping: %{category_col: :category_col, task_col: :task_col, start_col: :start_col, finish_col: :finish_col} )
-    plot_content = Contex.GanttChart.new(list_dataset)
+    # IO.inspect todays
+    IO.puts length(todays)
+    case length(todays) do
+      0 -> "Aucune tâche accomplie ce jour"
+      _ ->
 
 
-    plot = Contex.Plot.new(1200, 200, plot_content)
-    output = Contex.Plot.to_svg(plot)
+        data = Enum.map(todays, fn(task) -> %{category_col: task.priority_id,
+              task_col: task.title, start_col: date_to_datetime(task.date_start), finish_col: naive_to_datetime(task.achieved_at)} end)
+        dataset = Contex.Dataset.new(data)
+
+        list = Enum.map(todays, fn(task) -> [task.priority_id, task.title, date_to_datetime(task.date_start), naive_to_datetime(task.achieved_at)] end)
+        # list = [[]]
+        list_dataset = Contex.Dataset.new(list)
+        # IO.inspect list_dataset
+        # plot_content = Contex.GanttChart.new(dataset, mapping: %{category_col: :category_col, task_col: :task_col, start_col: :start_col, finish_col: :finish_col} )
+        plot_content = Contex.GanttChart.new(list_dataset)
+
+
+        plot = Contex.Plot.new(900, 600, plot_content)
+          |> Contex.Plot.titles("Diagramme de Gantt pour les tâches effectuées aujourd'hui", nil)
+        output = Contex.Plot.to_svg(plot)
+    end
+
   end
 
   def this_week_dataset do
-    todays = Monitoring.list_achieved_tasks_this_week
+    this_weeks = Monitoring.list_achieved_tasks_this_week
 
-    data = Enum.map(todays, fn(task) -> %{category_col: task.priority_id,
-          task_col: task.title, start_col: date_to_datetime(task.date_start), finish_col: naive_to_datetime(task.achieved_at)} end)
-    dataset = Contex.Dataset.new(data)
+    case length(this_weeks) do
+      0 -> "Aucune tâche accomplie cette semaine"
+      _ ->
+        data = Enum.map(this_weeks, fn(task) -> %{category_col: task.priority_id,
+        task_col: task.title, start_col: date_to_datetime(task.date_start), finish_col: naive_to_datetime(task.achieved_at)} end)
+        dataset = Contex.Dataset.new(data)
 
-    list = Enum.map(todays, fn(task) -> [task.priority_id, task.title, date_to_datetime(task.date_start), naive_to_datetime(task.achieved_at)] end)
+        list = Enum.map(this_weeks, fn(task) -> [task.priority_id, task.title, date_to_datetime(task.date_start), naive_to_datetime(task.achieved_at)] end)
 
-    list_dataset = Contex.Dataset.new(list)
-    IO.inspect list_dataset
-    # plot_content = Contex.GanttChart.new(dataset, mapping: %{category_col: :category_col, task_col: :task_col, start_col: :start_col, finish_col: :finish_col} )
-    plot_content = Contex.GanttChart.new(list_dataset)
+        list_dataset = Contex.Dataset.new(list)
+        # IO.inspect list_dataset
+        # plot_content = Contex.GanttChart.new(dataset, mapping: %{category_col: :category_col, task_col: :task_col, start_col: :start_col, finish_col: :finish_col} )
+        plot_content = Contex.GanttChart.new(list_dataset)
 
 
-    plot = Contex.Plot.new(1200, 600, plot_content)
-    output = Contex.Plot.to_svg(plot)
+        plot = Contex.Plot.new(900, 600, plot_content)
+          |> Contex.Plot.titles("Diagramme de Gantt pour les tâches effectuées cette semaine", nil)
+        output = Contex.Plot.to_svg(plot)
+    end
+
   end
 
+  @spec this_month_dataset :: {:safe, [...]}
   def this_month_dataset do
-    todays = Monitoring.list_achieved_tasks_this_month
+    this_months = Monitoring.list_achieved_tasks_this_month
 
-    data = Enum.map(todays, fn(task) -> %{category_col: task.priority_id,
-          task_col: task.title, start_col: date_to_datetime(task.date_start), finish_col: naive_to_datetime(task.achieved_at)} end)
-    dataset = Contex.Dataset.new(data)
-
-    list = Enum.map(todays, fn(task) -> [task.priority_id, task.title, date_to_datetime(task.date_start), naive_to_datetime(task.achieved_at)] end)
-
-    list_dataset = Contex.Dataset.new(list)
-    IO.inspect list_dataset
-    # plot_content = Contex.GanttChart.new(dataset, mapping: %{category_col: :category_col, task_col: :task_col, start_col: :start_col, finish_col: :finish_col} )
-    plot_content = Contex.GanttChart.new(list_dataset)
+    case length(this_months) do
+      0 -> "Aucune tâche accomplie ce mois"
+      _ ->
+        data = Enum.sort_by(this_months, fn(task) -> task.priority_id end)
+        |> Enum.map(fn(task) ->
+        [task.priority_id, task.title, date_to_naive(task.date_start), task.achieved_at]
+      end)
 
 
-    plot = Contex.Plot.new(1200, 800, plot_content)
-    output = Contex.Plot.to_svg(plot)
+      # IO.inspect data
+        dataset = Contex.Dataset.new(data, ["Cat", "Task", "Start", "End"])
+        plot_content = Contex.GanttChart.new(dataset)
+
+        plot = Contex.Plot.new(900, 600, plot_content)
+          |> Contex.Plot.titles("Diagramme de Gantt pour les tâches effectuées ce mois-ci", nil)
+
+          Contex.Plot.to_svg(plot)
+        # OLD DATASET
+        # data = Enum.map(this_months, fn(task) -> %{category_col: task.priority_id,
+        # task_col: task.title, start_col: date_to_datetime(task.date_start), finish_col: naive_to_datetime(task.achieved_at)} end)
+        # dataset = Contex.Dataset.new(data)
+
+        # list = Enum.map(this_months, fn(task) -> [task.priority_id, task.title, date_to_datetime(task.date_start), naive_to_datetime(task.achieved_at)] end)
+
+        # list_dataset = Contex.Dataset.new(list)
+        # IO.inspect list_dataset
+        # # plot_content = Contex.GanttChart.new(dataset, mapping: %{category_col: :category_col, task_col: :task_col, start_col: :start_col, finish_col: :finish_col} )
+        # plot_content = Contex.GanttChart.new(list_dataset)
+
+
+        # plot = Contex.Plot.new(1200, 800, plot_content)
+        # output = Contex.Plot.to_svg(plot)
+    end
+
+  end
+
+  def test_dataset do
+    this_months = Monitoring.list_achieved_tasks_this_month
+
+    case length(this_months) do
+      0 -> "Aucune tâche accomplie "
+      _ ->
+        data = Enum.sort_by(this_months, fn(task) -> task.priority_id end)
+          |> Enum.map(fn(task) ->
+          [task.priority_id, task.title, date_to_naive(task.date_start), task.achieved_at]
+        end)
+
+
+        # IO.inspect data
+        # dataset = Contex.Dataset.new(data, ["Cat", "Task", "Start", "End"])
+        # plot_content = Contex.GanttChart.new(dataset)
+
+        # plot = Contex.Plot.new(600, 400, plot_content)
+        #   |> Contex.Plot.titles("Sample Gantt Chart", nil)
+
+        #   Contex.Plot.to_svg(plot)
+
+        # list = Enum.map(this_months, fn(task) -> [task.priority_id, task.title, date_to_datetime(task.date_start), naive_to_datetime(task.achieved_at)] end)
+
+        # list_dataset = Contex.Dataset.new(list)
+        # IO.inspect list_dataset
+        # # plot_content = Contex.GanttChart.new(dataset, mapping: %{category_col: :category_col, task_col: :task_col, start_col: :start_col, finish_col: :finish_col} )
+        # plot_content = Contex.GanttChart.new(list_dataset)
+
+
+        # plot = Contex.Plot.new(1200, 800, plot_content)
+        # output = Contex.Plot.to_svg(plot)
+    end
+
   end
 
   def naive_to_datetime(naive) do
     {:ok, datetime} = DateTime.from_naive(naive, "Etc/UTC")
     datetime
+  end
+
+  def date_to_naive(date) do
+    {:ok, naive} = {Date.to_erl(date),{0,0,0}} |> NaiveDateTime.from_erl
+    naive
   end
 
   def date_to_datetime(date) do
