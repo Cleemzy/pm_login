4d6ebfa89b5a25d62f570fb643cd1ba8e0e3c76f
Cleemzy <razafintsalama.rmh@gmail.com>
2021-09-27 16:46:09 +0300

    added default value to identifiant

--- mix.exs
+++ mix.exs
@@ -33,24 +33,44 @@ defmodule PmLogin.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.0"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.7.0"},
-      {:postgrex, ">= 0.15.11"},
+      {:phoenix, "~> 1.5.9"},
+      {:phoenix_ecto, "~> 4.1"},
+      {:ecto_sql, "~> 3.4"},
+      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.3.3", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.5"},
-      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry, "~> 0.4"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_dashboard, "~> 0.4"},
+      {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
-      {:gettext, "~> 0.18"},
-      {:jason, "~> 1.2"},
-      {:mime, "~> 2.0"},
-      {:contex, git: "https://github.com/mindok/contex"},
-      {:plug_cowboy, "~> 2.5.2"},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
+      {:plug_cowboy, "~> 2.0"},
       {:comeonin, "~> 4.0"},
       {:bcrypt_elixir, "~> 1.0"},
-      {:phoenix_live_view, "~> 0.16.0"},
-      {:floki, ">= 0.31.0", only: :test}
+      {:phoenix_live_view, "~> 0.16"},
+      {:contex, git: "https://github.com/mindok/contex"},
+      {:floki, ">= 0.27.0", only: :test}
+
+      #changes needs to be fixed
+      # {:phoenix, "~> 1.6.0"},
+      # {:phoenix_ecto, "~> 4.4"},
+      # {:ecto_sql, "~> 3.7.0"},
+      # {:postgrex, ">= 0.15.11"},
+      # {:phoenix_html, "~> 3.0"},
+      # {:phoenix_live_reload, "~> 1.3.3", only: :dev},
+      # {:phoenix_live_dashboard, "~> 0.5"},
+      # {:telemetry_metrics, "~> 0.6"},
+      # {:telemetry_poller, "~> 0.4"},
+      # {:gettext, "~> 0.18"},
+      # {:jason, "~> 1.2"},
+      # {:mime, "~> 2.0"},
+      # {:contex, git: "https://github.com/mindok/contex"},
+      # {:plug_cowboy, "~> 2.5.2"},
+      # {:comeonin, "~> 4.0"},
+      # {:bcrypt_elixir, "~> 1.0"},
+      # {:phoenix_live_view, "~> 0.16.0"},
+      # {:floki, ">= 0.31.0", only: :test}
 
   #DEPS.UPDATE 18/06/2021
   #     cowboy 2.8.0 => 2.9.0
